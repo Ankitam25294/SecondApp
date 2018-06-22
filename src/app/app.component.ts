2449b47266f8e2b1252442d431b27b6c58fdc99a
Ankitam25294 <40385114+Ankitam25294@users.noreply.github.com>
2018-06-22 15:28:45 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`input[type=text]{
+    width: 250px;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    background-color: blue;
+}
+`]
 })
+
+/*
+//inline
+
+@Component({
+  selector: 'app-root',
+  template: `<div style="text-align:center;font-size:10mm;color:indigo">
+  Marvellous Infosystems<br>
+  <input type="text">
+</div>`,
+  styles: ['./app.component.css']
+})
+
+*/
 export class AppComponent {
-  title = 'app';
+  title = 'Marvellous Infosystems';
 }
